212306708dcafd92987c33b25ee8f8f31839a20d
Luiz Felipe Alves Rodrigues <luizrodriguescode@gmail.com>
2025-06-02 13:13:29 -0300

    Implementacao do listtile com card

--- angela_flutter/lib/main.dart
+++ angela_flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,19 +11,79 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(),
       home: SafeArea(
         child: Scaffold(
-          backgroundColor: Colors.teal,
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 50,
-                  backgroundImage: AssetImage('assets/images/foto.jpeg'),
+          backgroundColor: const Color.fromARGB(255, 1, 125, 113),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/images/foto.jpeg'),
+              ),
+              Text(
+                "Luiz",
+                style: TextStyle(
+                  fontSize: 60,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
                 ),
-              ],
-            ),
+              ),
+              Text(
+                "FLUTTER DEVELOPER",
+                style: GoogleFonts.sourceCodePro(
+                  color: Colors.teal.shade100,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  letterSpacing: 5,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: const Color.fromARGB(255, 0, 101, 91),
+                    ),
+                    title: Text(
+                      "+55 (99) 99103-2974",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.teal.shade900,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                // Este Card estava fora da lista de children da Column
+                margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: const Color.fromARGB(255, 1, 107, 96),
+                    ),
+                    title: Text(
+                      "yourmail@email.com",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.teal.shade900,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ], // Fechamento correto da lista de children
           ),
         ),
       ),
