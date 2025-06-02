c38c04477a9cf9f3582deff705f7a6a41baec554
Luiz Felipe Alves Rodrigues <luizrodriguescode@gmail.com>
2025-06-02 10:36:36 -0300

    Üpdate pratice

---
+++ angela_flutter/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.teal,
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('assets/images/foto.jpeg'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
