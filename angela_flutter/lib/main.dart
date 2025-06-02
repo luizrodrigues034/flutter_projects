a275919dd2dba5e19e6ce5b198a8e7056bcf6d07
Luiz Felipe Alves Rodrigues <luizrodriguescode@gmail.com>
2025-06-02 13:53:54 -0300

    feat: Botao entrar

--- angela_flutter/lib/main.dart
+++ angela_flutter/lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
           backgroundColor: const Color.fromARGB(255, 1, 125, 113),
           body: Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 50,
@@ -37,8 +38,13 @@ class MyApp extends StatelessWidget {
                   color: Colors.teal.shade100,
                   fontWeight: FontWeight.bold,
                   fontSize: 20,
-                  letterSpacing: 5,
+                  letterSpacing: 3,
                 ),
+              ),
+              SizedBox(
+                height: 20,
+                child: Divider(color: Colors.white),
+                width: 150,
               ),
               Card(
                 margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
@@ -46,6 +52,7 @@ class MyApp extends StatelessWidget {
                 child: Padding(
                   padding: const EdgeInsets.all(10.0),
                   child: ListTile(
+                    dense: true,
                     leading: Icon(
                       Icons.phone,
                       color: const Color.fromARGB(255, 0, 101, 91),
@@ -68,6 +75,7 @@ class MyApp extends StatelessWidget {
                 child: Padding(
                   padding: const EdgeInsets.all(10.0),
                   child: ListTile(
+                    dense: true,
                     leading: Icon(
                       Icons.email,
                       color: const Color.fromARGB(255, 1, 107, 96),
@@ -80,6 +88,21 @@ class MyApp extends StatelessWidget {
                         fontWeight: FontWeight.w400,
                       ),
                     ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 25),
+              Container(
+                width: 200,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Entrar",
+                    style: TextStyle(color: Colors.teal.shade900, fontSize: 20),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
                   ),
                 ),
               ),
