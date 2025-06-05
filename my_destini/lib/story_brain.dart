4a74d252d27cc957b71325fde4d51332ec0e1212
Luiz Felipe Alves Rodrigues <luizrodriguescode@gmail.com>
2025-06-05 16:12:43 -0300

    feat: Implementacao final do app destini, onde o usuario possui diversos caminhos

--- my_destini/lib/story_brain.dart
+++ my_destini/lib/story_brain.dart
@@ -4,50 +4,75 @@ class StoryBrain {
   final List<Story> _storyData = [
     Story(
       storyTitle:
-          'Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: "Need a ride, boy?".',
-      choice1: 'I\'ll hop in. Thanks for the help!',
-      choice2: 'Better ask him if he\'s a murderer first.',
-    ),
-    Story(
-      storyTitle: 'He nods slowly, unphased by the question.',
-      choice1: 'At least he\'s honest. I\'ll climb in.',
-      choice2: 'Wait, I know how to change a tire.',
+          'Seu carro furou um pneu em uma estrada sinuosa no meio do nada, sem sinal de celular. Você decide pedir carona. Uma caminhonete enferrujada para ao seu lado. Um homem com um chapéu de aba larga e olhos sem alma abre a porta do passageiro para você e pergunta: "Precisa de carona, garoto?".',
+      choice1: 'Vou entrar. Obrigado pela ajuda!',
+      choice2: 'É melhor perguntar se ele é um assassino primeiro.',
     ),
     Story(
       storyTitle:
-          'As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box.',
-      choice1: 'I love Elton John! Hand him the cassette tape.',
-      choice2: 'It\'s him or me! You take the knife and stab him.',
+          'Ele acena lentamente com a cabeça, inabalável pela pergunta.',
+      choice1: 'Pelo menos ele é honesto. Vou entrar.',
+      choice2: 'Espere, eu sei como trocar um pneu.',
     ),
     Story(
       storyTitle:
-          'What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?',
-      choice1: 'Restart',
+          'Ao começar a dirigir, o estranho começa a falar sobre seu relacionamento com a mãe. Ele fica cada vez mais irritado a cada minuto. Ele pede para você abrir o porta-luvas. Dentro você encontra uma faca ensanguentada, dois dedos decepados e uma fita cassete do Elton John. Ele estende a mão para o porta-luvas.',
+      choice1: 'Adoro Elton John! Entregue a fita cassete para ele.',
+      choice2: 'É ele ou eu! Você pega a faca e o esfaqueia.',
+    ),
+    Story(
+      storyTitle:
+          'O quê? Que desculpa esfarrapada! Você sabia que acidentes de trânsito são a segunda principal causa de morte acidental para a maioria dos grupos etários adultos?',
+      choice1: 'Reiniciar',
       choice2: '',
     ),
     Story(
       storyTitle:
-          'As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in.',
-      choice1: 'Restart',
+          'Enquanto você atravessa a grade de proteção e despenca em direção às rochas pontiagudas abaixo, você reflete sobre a duvidosa sabedoria de esfaquear alguém enquanto ele está dirigindo um carro em que você está.',
+      choice1: 'Reiniciar',
       choice2: '',
     ),
     Story(
       storyTitle:
-          'You bond with the murderer while crooning verses of "Can you feel the love tonight". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: "Try the pier".',
-      choice1: 'Restart',
+          'Você cria um laço com o assassino enquanto cantam versos de "Can you feel the love tonight". Ele te deixa na próxima cidade. Antes de você ir, ele pergunta se você conhece bons lugares para desovar corpos. Você responde: "Experimente o píer".',
+      choice1: 'Reiniciar',
       choice2: '',
     ),
   ];
+  int _storyNumber = 0;
+  void nextStory(int choiceNumber) {
+    if (_storyNumber == 0 && choiceNumber == 1) {
+      _storyNumber = 2;
+    } else if (_storyNumber == 2 && choiceNumber == 1) {
+      _storyNumber = 5;
+    } else if (_storyNumber == 2 && choiceNumber == 2) {
+      _storyNumber = 4;
+    } else if (_storyNumber == 0 && choiceNumber == 2) {
+      _storyNumber = 1;
+    } else if (_storyNumber == 1 && choiceNumber == 1) {
+      _storyNumber = 2;
+    } else if (_storyNumber == 1 && choiceNumber == 2) {
+      _storyNumber = 3;
+    }
+  }
 
   getTitleStory() {
-    return _storyData[0].storyTitle;
+    return _storyData[_storyNumber].storyTitle;
   }
 
   getChoice1() {
-    return _storyData[0].choice1;
+    return _storyData[_storyNumber].choice1;
   }
 
   getChoice2() {
-    return _storyData[0].choice2;
+    return _storyData[_storyNumber].choice2;
+  }
+
+  int getActualStory() {
+    return _storyNumber;
+  }
+
+  setRestart() {
+    _storyNumber = 0;
   }
 }
