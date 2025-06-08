56c7f1098e08d5c8bba5365767c8ad7fd23cc770
Luiz Felipe Alves Rodrigues <luizrodriguescode@gmail.com>
2025-06-08 10:36:49 -0300

    feat: cards

--- imc_calc/lib/main.dart
+++ imc_calc/lib/main.dart
@@ -1,43 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:imc_calc/input_page.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(
+      theme: ThemeData.dark().copyWith(
         appBarTheme: AppBarTheme(
           backgroundColor: Color(0xFF0C101F),
-          elevation: 2,
-          titleTextStyle: TextStyle(color: Colors.white, fontSize: 29),
+          elevation: 1,
         ),
-        textTheme: TextTheme(bodyLarge: TextStyle(color: Colors.white)),
         scaffoldBackgroundColor: Color(0xFF0C101F),
-        floatingActionButtonTheme: FloatingActionButtonThemeData(
-          backgroundColor: Color.fromARGB(255, 255, 255, 255),
-        ),
       ),
-      home: InputPage(),
+      home: CardGender(),
     ),
   );
 }
 
-class InputPage extends StatefulWidget {
-  const InputPage({super.key});
-
+class CardGender extends StatefulWidget {
+  const CardGender({super.key});
   @override
-  State<InputPage> createState() => _InputPageState();
+  State<CardGender> createState() => _CardGenderState();
 }
 
-class _InputPageState extends State<InputPage> {
+class _CardGenderState extends State<CardGender> {
+  Color selecionadoMan = Color.fromARGB(255, 18, 24, 42);
+  Color selecionadoWoman = Color.fromARGB(255, 18, 24, 42);
+  selectGender(int gender) {
+    if (gender == 1) {
+      setState(() {
+        selecionadoWoman = const Color.fromARGB(255, 27, 27, 27);
+        selecionadoMan = Color.fromARGB(255, 18, 24, 42);
+      });
+    } else {
+      setState(() {
+        selecionadoMan = const Color.fromARGB(255, 27, 27, 27);
+        selecionadoWoman = Color.fromARGB(255, 18, 24, 42);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Imc Calculator")),
-      body: Center(
-        child: Text('Ola', style: Theme.of(context).textTheme.bodyLarge),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {},
+      appBar: AppBar(title: Text('Imc Calc')),
+      body: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                color: selecionadoMan,
+                child: InkWell(
+                  onTap: () => selectGender(0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(Icons.male, semanticLabel: 'Male', size: 100),
+                        Text('MALE'),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                color: selecionadoWoman,
+                child: InkWell(
+                  onTap: () => selectGender(1),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(Icons.female, semanticLabel: 'Male', size: 100),
+                        Text('FEMALE'),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
