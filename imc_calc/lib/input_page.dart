72091b77923cadc010b5e437df1e34f788a0caed
Luiz Felipe Alves Rodrigues <luizrodriguescode@gmail.com>
2025-06-08 15:58:30 -0300

    feat: response layout with expandeden

--- imc_calc/lib/input_page.dart
+++ imc_calc/lib/input_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const Color colorCard = Color(0xFF1D1E33);
+const bottomContainerColour = Color(0xFFEB1555);
+const double heigthBottom = 80;
+const double iconCardSize = 80;
 
 class InputPage extends StatefulWidget {
   const InputPage({super.key});
@@ -12,11 +18,98 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Imc Calculator")),
-      body: Center(child: Text('Ola')),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {},
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    selectColor: colorCard,
+                    chieldContent: GenderProeperties(
+                      icone: FontAwesomeIcons.mars,
+                      genero: 'MALE',
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    selectColor: colorCard,
+                    chieldContent: GenderProeperties(
+                      icone: FontAwesomeIcons.venus,
+                      genero: 'FEMALE',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(child: ReusableCard(selectColor: colorCard)),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(child: ReusableCard(selectColor: colorCard)),
+                Expanded(child: ReusableCard(selectColor: colorCard)),
+              ],
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            margin: EdgeInsets.only(top: 10),
+            height: heigthBottom,
+            color: bottomContainerColour,
+            child: Center(
+              child: Text(
+                'RESULTADO',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+              ),
+            ),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class GenderProeperties extends StatelessWidget {
+  final IconData icone;
+  final String genero;
+  const GenderProeperties({required this.icone, required this.genero});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icone, size: iconCardSize),
+        SizedBox(height: 15),
+        Text(
+          genero,
+          style: TextStyle(
+            fontSize: 18,
+            color: Color.fromARGB(145, 248, 216, 233),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({required this.selectColor, this.chieldContent});
+
+  final Color selectColor;
+  final Widget? chieldContent;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: selectColor,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: chieldContent,
     );
   }
 }
--- imc_calc/lib/main.dart
+++ imc_calc/lib/main.dart
@@ -23,72 +23,8 @@ class CardGender extends StatefulWidget {
 }
 
 class _CardGenderState extends State<CardGender> {
-  Color selecionadoMan = Color.fromARGB(255, 18, 24, 42);
-  Color selecionadoWoman = Color.fromARGB(255, 18, 24, 42);
-  selectGender(int gender) {
-    if (gender == 1) {
-      setState(() {
-        selecionadoWoman = const Color.fromARGB(255, 27, 27, 27);
-        selecionadoMan = Color.fromARGB(255, 18, 24, 42);
-      });
-    } else {
-      setState(() {
-        selecionadoMan = const Color.fromARGB(255, 27, 27, 27);
-        selecionadoWoman = Color.fromARGB(255, 18, 24, 42);
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Imc Calc')),
-      body: Row(
-        children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Card(
-                color: selecionadoMan,
-                child: InkWell(
-                  onTap: () => selectGender(0),
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(Icons.male, semanticLabel: 'Male', size: 100),
-                        Text('MALE'),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Card(
-                color: selecionadoWoman,
-                child: InkWell(
-                  onTap: () => selectGender(1),
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(Icons.female, semanticLabel: 'Male', size: 100),
-                        Text('FEMALE'),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+    return InputPage();
   }
 }
